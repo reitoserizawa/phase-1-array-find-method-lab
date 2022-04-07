2c601253a6b6ffd5fec56fc1ae3f4bc3d73c2b1d
ReitoSerizawa <s.reitiger@gmail.com>
2022-04-06 23:35:43 -0400

    commit tree e82f5c4c3060b39260a8d25bc90ce6816cb24d35

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+// function superbowlWin (record) {
+//     let winYear = record.find(record => record.result === 'W')
+//     return winYear.year
+// }
+
+
+function superbowlWin(record) {
+    const foundRecord = record.find(records => records.result === "W")
+    return foundRecord && foundRecord.year
+}
